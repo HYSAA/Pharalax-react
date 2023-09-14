fe7874f2973e39d94cb25d3cb5e8128ddd4afdcc
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-15 04:11:52 +0800

    change:

--- pharalax/src/App.jsx
+++ pharalax/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './App.css'
-// import earth from './assets/earth.jpg'
+import earth from './assets/Earth.png'
 // import moon from './assets/moon.jpg'
-import trees from './assets/Trees.png'
-import mountain from './assets/Mountain.png'
+// import trees from './assets/Trees.png'
+// import mountain from './assets/Mountain.png'
+import moon from './assets/MOON.png'
+import space from './assets/SPACE.png'
 
 function Header(){
 return(
+  <div className='page'>
   <div className='wrapper'>
   <header>
-  <img src={mountain} alt='mountain' className='background'/>
-  <img src={trees} alt="trees" className='foreground' />
+  <img src={space} alt='mountain' className='background'/>
+  <img src={earth} alt="trees" className='foreground' />
   <h1 className='title'>WELCOME!!!</h1>
   </header>
   <section>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -28,6 +31,7 @@ return(
        illo aliquam pariatur veritatis deserunt quaerat eos fuga sequi. Ex, non reiciendis 
        repellat modi, voluptate distinctio dolor accusantium, magnam praesentium impedit excepturi fuga alias! Voluptatibus corrupti beatae reprehenderit doloribus ipsum sapiente ut, velit ab placeat et laborum dolorem inventore facilis nulla voluptatum doloremque deleniti quos magnam quibusdam! Et in provident accusamus quis! Perferendis quas eius accusamus cumque architecto sed reprehenderit quis tenetur! Sequi fugiat saepe suscipit beatae at totam ut? Enim deserunt corrupti maxime laboriosam quam soluta minima harum provident, doloremque cumque eaque officiis, omnis, architecto molestiae. Aut odit vel dolor quibusdam dignissimos error quae possimus, explicabo libero quidem laboriosam, repellat corrupti reprehenderit blanditiis excepturi cupiditate, sequi provident adipisci consectetur! Magni sint, impedit eius dicta a saepe sed soluta quisquam accusamus. Dicta facere repudiandae inventore enim nam dolorum amet ipsa consequuntur ad recusandae distinctio, laboriosam voluptatibus aspernatur laudantium illum similique. Assumenda nesciunt porro praesentium quod eum delectus, neque quam corporis molestiae distinctio deserunt.</section>
   </div>
+  </div>
 )
 }
 
@@ -36,8 +40,9 @@ function App() {
 
   return (
    <div>
+
     <Header></Header>
-  
+
    </div>
   )
 }
